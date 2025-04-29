05d000d6d0042c60a593b4da30430faafc08e297
Juliette Cordor <me@cordor.dev>
2025-04-29 11:29:58 +1000

    ugh

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,13 +1,43 @@
 use gtk::{Application, ApplicationWindow, Button, glib, prelude::*};
+use maestro::{hdlc::Codec, protocol::utils, pwrpc::client::Client};
 
 const APP_ID: &str = "com.cordor.pbpctrlgui";
 
-fn main() -> glib::ExitCode {
+mod bt;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let app = Application::builder().application_id(APP_ID).build();
+
+    // set up session
+    let session = bluer::Session::new().await?;
+    let adapter = session.default_adapter().await?;
+
+    // set up device
+    let dev = {
+        tracing::debug!("no device specified, searching for compatible one");
+        bt::find_maestro_device(&adapter).await?
+    };
+
+    // set up profile
+    let stream = bt::connect_maestro_rfcomm(&session, &dev).await?;
+
+    // set up codec
+    let codec = Codec::new();
+    let stream = codec.wrap(stream);
+
+    // set up RPC client
+    let mut client = Client::new(stream);
+    let handle = client.handle();
+
+    // resolve channel
+    let channel = utils::resolve_channel(&mut client).await?;
 
     app.connect_activate(build_ui);
 
-    app.run()
+    app.run();
+
+    Ok(())
 }
 
 fn build_ui(app: &Application) {
